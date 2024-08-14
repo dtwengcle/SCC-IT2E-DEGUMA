6791ca80bf4526225c3aaf48b8ef809f1c5e7325
dtwengcle <dtwengcle@gmail.com>
2024-08-14 17:52:29 +0800

    dont open this file, pinaghihirpan koto

---
+++ src/degumae/DegumaE.java
@@ -0,0 +1,18 @@
+
+package degumae;
+
+import java.util.Scanner;
+public class DegumaE {
+ 
+    
+    public static void main(String[] args) {
+        
+        
+        
+        sales sls = new sales();
+        sls.getSales();
+        
+    }
+    
+    
+}
